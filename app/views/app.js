bd9060707e8f897a292c2c9f096628c373a544c2
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-07-19 09:04:32 +0000

    変数例

--- app/views/app.js
+++ app/views/app.js
@@ -1,4 +1,20 @@
 // alert("Hello World");
 
-let hello = "Hello World";
-alert(hello);
+// let hello = "Hello World";
+// alert(hello);
+
+// 整数
+let int1 = 1;
+alert(int1);
+
+// 負数
+let int2 = -10;
+alert(int2);
+
+// 小数点
+let float1 = 3.14;
+alert(float1);
+
+// 文字列
+let str1 = "JavaScriptを覚えよう";
+alert(str1);
