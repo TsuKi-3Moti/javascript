1b59cd584b93cdf19bd0fbda5d16f687ffd93d50
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-07-19 09:31:10 +0000

    条件式

--- app/views/app.js
+++ app/views/app.js
@@ -4,38 +4,52 @@
 // alert(hello);
 
 // 整数
-let int1 = 1;
-alert(int1);
+// let int1 = 1;
+// alert(int1);
 
-// 負数
-let int2 = -10;
-alert(int2);
+// // 負数
+// let int2 = -10;
+// alert(int2);
 
-// 小数点
-let float1 = 3.14;
-alert(float1);
+// // 小数点
+// let float1 = 3.14;
+// alert(float1);
 
-// 文字列
-let str1 = "JavaScriptを覚えよう";
-alert(str1);
+// // 文字列
+// let str1 = "JavaScriptを覚えよう";
+// alert(str1);
 
-// 足し算
-alert(4 + 3);
+// // 足し算
+// alert(4 + 3);
 
-// 引き算
-alert(8 - 5);
+// // 引き算
+// alert(8 - 5);
 
-// 掛け算
-alert(2 * 6);
+// // 掛け算
+// alert(2 * 6);
 
-// 割り算
-alert(10 / 3);
+// // 割り算
+// alert(10 / 3);
 
-// 文字列を結合
-alert("Hello" + 'World');
+// // 文字列を結合
+// alert("Hello" + 'World');
 
-// 変数を利用する場合
-let str3 = 'Hello';
-let str4 = 'World!!';
-alert(str3 + str4);
+// // 変数を利用する場合
+// let str3 = 'Hello';
+// let str4 = 'World!!';
+// alert(str3 + str4);
 
+let orange = 100;
+let apple = 120;
+
+// if(orange < apple){alert("みかんの値段がリンゴより安い");}
+// else if(orange == apple){alert("みかんとリンゴが同じ値段")}
+// else {alert("みかんの値段がリンゴより高い");}
+
+if(orange < apple){
+  alert("みかんの値段がリンゴより安い");
+} else if(orange == apple){
+  alert("みかんとリンゴが同じ値段")
+} else{
+  alert("みかんの値段がリンゴより高い");
+}
